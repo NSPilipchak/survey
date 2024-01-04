1c6745d8c2f92a635bf5a42dd54af239a0882a79
NSPilipchak <kolich@ua.fm>
2024-01-04 12:58:17 +0200

    added loadingQuestions page

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import "./App.css";
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import Emploees from "./pages/Emploees";
+import LoadQuestions from "./pages/LoadQuestions";
 
 const App = () => (
   <div>
     <Routes>
       <Route exact path="/" element={<IndexPage />} />
       <Route path="/employees" element={<Emploees />} />
+      <Route path="/load" element={<LoadQuestions />} />
     </Routes>
   </div>
 );
 
 const IndexPage = () => {
   return (
-    <h3>
-      Let's go to <Link to="/employees">Emploees</Link>
-    </h3>
+    <div>
+      <h3>
+        Load the <Link to="/load">questions</Link>
+      </h3>
+      <h3>
+        Let's go to <Link to="/employees">Emploees</Link>
+      </h3>
+    </div>
   );
 };
 
---
+++ src/pages/LoadQuestions.js
@@ -0,0 +1,73 @@
+import React, {useState} from "react";
+import { Link } from "react-router-dom";
+import * as XLSX from "xlsx";
+
+const LoadQuestions = () => {
+
+  const [fileName, setfileName] = useState(null);
+  const [loadedFile, setLoadedFile] = useState(localStorage.getItem("fileName") || "");
+  const [lastModifiedDate, setLastModifiedDate] = useState(localStorage.getItem("lastModifiedDate") || "");
+  const [numberOfQuestions, setNumberOfQuestions] = useState(localStorage.getItem("NumberOfQuestions") || "");
+
+  const handleFile = async (e) => {
+    const file = e.target.files[0];
+    setfileName(file.name);
+    const data = await file.arrayBuffer();
+    const workbook = XLSX.read(data);
+    
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet);
+    
+    localStorage.setItem("questions", JSON.stringify(jsonData));
+    localStorage.setItem("NumberOfQuestions", jsonData.length);
+    setNumberOfQuestions(jsonData.length);
+    localStorage.setItem("fileName", file.name);
+    setLoadedFile(file.name);
+    localStorage.setItem("lastModifiedDate", JSON.stringify(file.lastModifiedDate));
+    setLastModifiedDate(JSON.stringify(file.lastModifiedDate));
+    console.log(jsonData);
+  }
+
+
+  return (
+    <section className="main">
+      <div>
+      <p>Loaded questions:</p>
+        {loadedFile && (
+          <p>
+            File Name: <span>{loadedFile}</span>
+          </p>
+        )}
+        {loadedFile && (
+          <p>
+            Number Of Questions : <span>{numberOfQuestions}</span>
+          </p>
+        )}
+        {loadedFile && (
+          <p>
+            Last Modified Date : <span>{lastModifiedDate}</span>
+          </p>
+        )}
+      </div>
+      <div className="alphabet">
+        <p>Load new questions:</p>
+        {fileName && (
+          <p>
+            FileName: <span>{fileName}</span>
+          </p>
+        )}
+        <input
+          type="file"
+          onInput={(e) => handleFile(e)}
+        />
+      </div>
+      <div>
+        <h3>
+          Let's go to <Link to="/">main page</Link>
+        </h3>
+      </div>
+    </section>
+  );
+};
+
+export default LoadQuestions;
