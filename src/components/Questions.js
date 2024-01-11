ebb3ddf6e74364576bd0bf7c06ce32e0c6534e1a
NSPilipchak <kolich@ua.fm>
2024-01-11 15:00:43 +0200

    added next and previous buttons

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import Question from "../components/Question";
 import { setAnswerKey } from "../helpers/Helpers";
 
 const Questions = ({answerPool, setAnswerPool, questions = [], options}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showQuestions] = useState([questions.length]);
+  const show = () => {
+    questions.map((q, index) => (
+      showQuestions[index] = index === currentIndex
+    ));
+  }
+  show();
 
   const handleSelectAnswer = (id, key, countOfAnswer) => {
     if (answerPool.find(item => item.Id === id)) {
@@ -23,10 +31,30 @@ const Questions = ({answerPool, setAnswerPool, questions = [], options}) => {
     }
   };
 
+  const getStyle = (index) => {
+    return showQuestions[index] ? '' : 'hideQuestion';
+  }
+
+  function pressPrevious() {
+    if (currentIndex > 0 ) {
+      showQuestions[currentIndex] = false;
+      setCurrentIndex(currentIndex - 1);
+      showQuestions[currentIndex] = true;
+    }
+  }
+
+  function pressNext() {
+    if (currentIndex < showQuestions.length - 1) {
+      showQuestions[currentIndex] = false;
+      setCurrentIndex(currentIndex + 1);
+      showQuestions[currentIndex] = true;
+    }
+  }
+
   return (
     <div className="column_question_list">
       {questions.map((question, index) => (
-        <div key={`question_index${index}`}>
+        <div key={`question_index${index}`} className={getStyle(index)}>
           <Question
             key={index}
             item={question}
@@ -34,6 +62,8 @@ const Questions = ({answerPool, setAnswerPool, questions = [], options}) => {
             setAnswerPool={handleSelectAnswer}
             options={options}
           />
+          <button onClick={pressPrevious}>Previous</button>
+          <button onClick={pressNext}>Next</button>
         </div>
       ))}
     </div>
